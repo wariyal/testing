9f135aca028d8c00ca05bfadf8816721b17a5ca5
wariyal <wariyalmmn25@gmail.com>
2024-12-07 10:36:27 +0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SurveyController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::apiResource('survey', SurveyController::class);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+});
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/survey/get-by-slug/{survey:slug}', [SurveyController::class, 'getBySlug']);
+Route::post('/survey/{survey}/answer', [SurveyController::class, 'storeAnswer']);
